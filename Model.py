f99d38389b11e34154e2422f47b65c1e0e93d10a
tomfriwel <976932447@qq.com>
2018-08-16 10:06:29 +0800

    comment

--- Model.py
+++ Model.py
@@ -11,5 +11,8 @@ class Model(object):
         self.db = None
         
     def initdb(self):
+        """
+            init db with dbn[mysql], db(database name), user, password
+        """
         if self.db is None and self.dbn is not None:
             self.db = web.database(dbn=config.db['dbn'], db=self.dbn, user=config.db['user'], pw=config.db['pw'])
